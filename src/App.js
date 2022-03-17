5b46086e04d2068e977cf5ef103e7a1997cb9a19
0bserver <rlee767@gmail.com>
2022-03-17 20:41:40 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchBar from "./component/SearchBar";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Aggregator</h1>
+      <Input />
+      <Input />
+      <Button />
+      <Button />
+    </div>
+  );
+};
+
+export default App;
